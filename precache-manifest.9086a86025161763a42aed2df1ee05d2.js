51f2dd8e8d1550243c21c2a9f6eeb9c3b324e16b
boburazimov <uzbek.4103@gmail.com>
2021-03-02 20:09:29 +0500

    Updates

---
+++ precache-manifest.9086a86025161763a42aed2df1ee05d2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ccc64c01ff5893a0e5507c507ec275b",
+    "url": "/app-react-samurai/index.html"
+  },
+  {
+    "revision": "04679f602f2c1af01ded",
+    "url": "/app-react-samurai/static/css/main.1d0fe784.chunk.css"
+  },
+  {
+    "revision": "83414c7f827be2a0734f",
+    "url": "/app-react-samurai/static/js/2.1785d8f5.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/app-react-samurai/static/js/2.1785d8f5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "04679f602f2c1af01ded",
+    "url": "/app-react-samurai/static/js/main.1b7468cb.chunk.js"
+  },
+  {
+    "revision": "f56a6f7241dd091f1105",
+    "url": "/app-react-samurai/static/js/runtime-main.70a67de5.js"
+  },
+  {
+    "revision": "fa67e6b5382ae9bfd19ebdf228345660",
+    "url": "/app-react-samurai/static/media/Preloader.fa67e6b5.svg"
+  },
+  {
+    "revision": "e6945badfcff4a2fdb7e5c0bea9cae12",
+    "url": "/app-react-samurai/static/media/user.e6945bad.png"
+  }
+]);
